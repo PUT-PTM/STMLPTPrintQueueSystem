04a5e886bef3bca64a0e2c84151a393e126de4b8
Norbert Langner <norbitor@gmail.com>
2016-04-28 21:37:09 +0200

    STM: Receiving data from RPi using external interrupts

--- hwlayer/usart_link.c
+++ hwlayer/usart_link.c
@@ -2,15 +2,18 @@
 #include "stm32f4xx_usart.h"
 #include "stm32f4xx_conf.h"
 #include "stm32f4xx_rcc.h"
+#include "misc.h"
+#include "stm32f4xx_exti.h"
 #include "usart_link.h"
 
 void usart_configure()
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
 
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3); // Tx Line
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3); // Rx Line
+	// GPIO Pins configuration
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_UART4); // Tx Line
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_UART4); // Rx Line
 
 	GPIO_InitTypeDef txrx_lines;
 	txrx_lines.GPIO_OType = GPIO_OType_PP;
@@ -18,8 +21,9 @@ void usart_configure()
 	txrx_lines.GPIO_Mode = GPIO_Mode_AF;
 	txrx_lines.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
 	txrx_lines.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOD, &txrx_lines);
+	GPIO_Init(GPIOC, &txrx_lines);
 
+	// UART parameters setup
 	USART_InitTypeDef usart;
 	usart.USART_BaudRate = 9600;
 	usart.USART_WordLength = USART_WordLength_8b;
@@ -28,17 +32,40 @@ void usart_configure()
 	usart.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 	usart.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-	USART_Init(USART3, &usart);
-	USART_Cmd(USART3, ENABLE);
+	USART_Init(UART4, &usart);
+	USART_Cmd(UART4, ENABLE);
+
+	// Receiving interrupt configuration
+	NVIC_InitTypeDef usart_nvic;
+	USART_ITConfig(UART4, USART_IT_RXNE, ENABLE);
+	usart_nvic.NVIC_IRQChannel = UART4_IRQn;
+	usart_nvic.NVIC_IRQChannelPreemptionPriority = 0;
+	usart_nvic.NVIC_IRQChannelSubPriority = 0;
+	usart_nvic.NVIC_IRQChannelCmd = ENABLE;
+
+	NVIC_Init(&usart_nvic);
+	NVIC_EnableIRQ(UART4_IRQn);
 }
 
 void send_char(char c)
 {
-	while (USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
-	USART_SendData(USART3, c);
+	while (USART_GetFlagStatus(UART4, USART_FLAG_TXE) == RESET);
+	USART_SendData(UART4, c);
 }
 
 void send_string(const char* s)
 {
 	while(*s) send_char(*s++);
+}
+
+void UART4_IRQHandler(void)
+{
+	if (USART_GetITStatus(UART4, USART_IT_RXNE) != RESET)
+	{
+		if(UART4->DR == '1') {
+			GPIO_SetBits(GPIOD, GPIO_Pin_12);
+		} else if(UART4->DR == '0') {
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+		}
+	}
 }
--- hwlayer/usart_link.h
+++ hwlayer/usart_link.h
@@ -1,3 +1,4 @@
+// Configuration
 void usart_configure();
 void send_char(char c);
 void send_string(const char* s);
