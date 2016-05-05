57fef0dff23276ba731ed21dfea1d4fa73dda8de
Norbert Langner <norbitor@gmail.com>
2016-05-05 23:27:06 +0200

    RPi: Receiving and sending string lines

--- hwrecsrv/uart_recv_rpi.py
+++ hwrecsrv/uart_recv_rpi.py
@@ -2,6 +2,14 @@ import serial
 
 port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 
+port.flushInput()
+port.flushOutput()
+
+rec = port.readline()
+if rec == "HELLO STM\n":
+    print("Connected with STM Board!")
+
 while True:
-    rcv = port.read(10)
+    port.write("echo echo\n")
+    rcv = port.readline()
     print(rcv)
