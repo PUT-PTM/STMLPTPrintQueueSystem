33b2611d4da03a6d8364ca5c25f00be6990a95bf
Norbert Langner <norbitor@gmail.com>
2016-05-21 18:06:41 +0200

    Web: Admin, adding new categories

--- websys/src/AppBundle/Controller/Admin/CategoriesController.php
+++ websys/src/AppBundle/Controller/Admin/CategoriesController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\CaseCategory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoriesController extends Controller
 {
@@ -14,7 +18,34 @@ class CategoriesController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $categories = $em->getRepository('AppBundle:CaseCategory')->findAll();
-        
+
         return $this->render('admin/categories.html.twig', array('categories' => $categories));
+    }
+
+    /**
+     * @Route("/categories/add")
+     */
+    public function addAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $category = new CaseCategory();
+
+        $form = $this->createFormBuilder($category)
+                ->add('name', TextType::class)
+                ->add('shortcut', TextType::class)
+                ->add('save', SubmitType::class, array('label' => 'Create Category'))
+                ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+
+            $this->addFlash('notice', 'Saved new category with id ' . $category->getId() . '!');
+            return $this->redirectToRoute('app_admin_categories_show');
+        }
+
+        return $this->render('admin/new_category.html.twig', array('form' => $form->createView()));
     }
 }
