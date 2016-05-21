c0ea71d4d5cba261694bfe0b50c241391f0dbc9a
Norbert Langner <norbitor@gmail.com>
2016-05-21 22:08:37 +0200

    Web: Admin, user locking and unlocking

--- websys/src/AppBundle/Controller/Admin/UsersController.php
+++ websys/src/AppBundle/Controller/Admin/UsersController.php
@@ -2,12 +2,8 @@
 
 namespace AppBundle\Controller\Admin;
 
-use AppBundle\Entity\CaseCategory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UsersController extends Controller
 {
@@ -20,5 +16,41 @@ class UsersController extends Controller
         $users = $em->getRepository('AppBundle:User')->findAll();
 
         return $this->render('admin/users.html.twig', array('users' => $users));
+    }
+
+    /**
+     * @Route("/users/lock/{id}")
+     */
+    public function lockAction($id)
+    {
+        if ($id == $this->getUser()->getId()) {
+            $this->addFlash('error', 'You cannot lock yourself!');
+        } else {
+            $userManager = $this->get('fos_user.user_manager');
+            $user = $userManager->findUserBy(array('id'=>$id));
+
+            $user->setLocked(true);
+
+            $userManager->updateUser($user);
+
+            $this->addFlash('notice', 'User ' . $user->getUsername() . ' has been locked!');
+        }
+        return $this->redirectToRoute('app_admin_users_show');
+    }
+
+    /**
+     * @Route("/users/unlock/{id}")
+     */
+    public function unlockAction($id)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('id'=>$id));
+
+        $user->setLocked(false);
+
+        $userManager->updateUser($user);
+
+        $this->addFlash('notice', 'User ' . $user->getUsername() . ' has been unlocked!');
+        return $this->redirectToRoute('app_admin_users_show');
     }
 }
