3adc3efd2be6a66740a6fd9fe89c0cc4d9d346cf
Norbert Langner <norbitor@gmail.com>
2016-05-22 15:05:11 +0200

    Web: Admin, position add and edit

--- websys/src/AppBundle/Controller/Admin/PositionsController.php
+++ websys/src/AppBundle/Controller/Admin/PositionsController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\Position;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PositionsController extends Controller
 {
@@ -17,5 +21,62 @@ class PositionsController extends Controller
         $positions = $em->getRepository('AppBundle:Position')->findAll();
 
         return $this->render('admin/positions.html.twig', array('positions' => $positions));
+    }
+
+    /**
+     * @Route("/positions/add")
+     */
+    public function addAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $position = new Position();
+
+        $form = $this->getFormForPosition($position);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($position);
+            $em->flush();
+
+            $this->addFlash('notice', 'Saved new position with id ' . $position->getId() . '!');
+            return $this->redirectToRoute('app_admin_positions_show');
+        }
+
+        return $this->render('admin/category_new.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/positions/edit/{id}")
+     */
+    public function editAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $position = $em->getRepository('AppBundle:Position')->findOneById($id);
+        if (!$position) {
+            throw $this->createNotFoundException('No position found for id: ' . $id);
+        }
+
+        $form = $this->getFormForPosition($position, true);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($position);
+            $em->flush();
+
+            $this->addFlash('notice', 'Edited position with id ' . $position->getId() . '!');
+            return $this->redirectToRoute('app_admin_positions_show');
+        }
+
+        return $this->render('admin/category_edit.html.twig', array('form' => $form->createView()));
+    }
+
+    private function getFormForPosition(Position $position, $edit = false)
+    {
+        $formBuilder = $this->createFormBuilder($position)
+               ->add('name', TextType::class)
+               ->add('save', SubmitType::class, array('label' => 'Save'));
+        return $formBuilder->getForm();
     }
 }
