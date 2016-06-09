13ffd2a548f529402677fb23995faa7ec1d3cfd5
Norbert Langner <norbitor@gmail.com>
2016-06-09 18:23:16 +0200

    STM: Secondary UART for debugging purposes (PD5,6)

--- hwlayer/usart_link.c
+++ hwlayer/usart_link.c
@@ -52,10 +52,46 @@ void usart_configure()
 	RcvBuffReset(&uart_buffer);
 }
 
+void usart2_configure()
+{
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	// GPIO Pins configuration
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource5, GPIO_AF_USART2); // Tx Line
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource6, GPIO_AF_USART2); // Rx Line
+
+	GPIO_InitTypeDef txrx_lines;
+	txrx_lines.GPIO_OType = GPIO_OType_PP;
+	txrx_lines.GPIO_PuPd = GPIO_PuPd_UP;
+	txrx_lines.GPIO_Mode = GPIO_Mode_AF;
+	txrx_lines.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6;
+	txrx_lines.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &txrx_lines);
+
+	// UART parameters setup
+	USART_InitTypeDef usart;
+	usart.USART_BaudRate =9600;
+	usart.USART_WordLength = USART_WordLength_8b;
+	usart.USART_StopBits = USART_StopBits_1;
+	usart.USART_Parity = USART_Parity_No;
+	usart.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	usart.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	USART_Init(USART2, &usart);
+	USART_Cmd(USART2, ENABLE);
+}
+
 void send_char(char c)
 {
 	while (USART_GetFlagStatus(UART4, USART_FLAG_TXE) == RESET);
 	USART_SendData(UART4, c);
+}
+
+void send_char_2(char c)
+{
+	while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+	USART_SendData(USART2, c);
 }
 
 void send_string(const char* s)
@@ -75,6 +111,7 @@ void UART4_IRQHandler(void)
 {
 	if (USART_GetITStatus(UART4, USART_IT_RXNE) != RESET)
 	{
+		send_char_2(UART4->DR);
 		if(uart_buffer.ready == 0) {
 			if( UART4->DR == '\n' && uart_buffer.buffer[uart_buffer.current_pos-1] == '\r') {
 				uart_buffer.ready = 1;
