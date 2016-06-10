840c1d3a2a06641a216898daeaa94b4ab6b36f6e
jan smigielski <jan smigielski>
2016-06-10 11:11:20 +0200

    not working lib

--- hwlayer/main.c
+++ hwlayer/main.c
@@ -5,19 +5,25 @@
 #include "esp8266_at_parser/esp8266.h"
 
 
-
+ESP8266_Result_t result;
 int main(void)
 {
 	SystemInit();
 	TM_DELAY_Init();
 	discovery_led_configure();
-
+	set_orange_led_on();
 	ESP8266_t esp;
-	ESP8266_Result_t result;
+
 	result = ESP8266_Init(&esp, 9600);
+	result = ESP8266_StartClientConnectionTCP(&esp,	"ubuntu.local",	"192.168.0.105",80,"");
+	ESP8266_Connection_t con = esp.Connection[0];
+	con.Data="GET \/ HTTP\/1.0 HOST: 192.168.0.108 Connection: keep-alive Accept: *\/*";
+	result = ESP8266_RequestSendData(&esp, &con);
+
 	if (result == ESP_OK) {
 		set_green_led_on();
 	} else {
 		set_red_led_on();
 	}
+
 }
